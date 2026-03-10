d487adde90cb8d0b01d8a004d4113bda6e57c336
Balder <balder.ansgar@gmail.com>
2026-03-10 16:54:45 -0300

    Atualizar + listar adicionado

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Aluno(Base):
     id = Column(Integer, primary_key=True)
     nome = Column(String(100))
     idade = Column(Integer)
-    Curso = Column(String(150))
+    curso = Column(String(150))
 
     def __init__(self, nome, idade, curso):
         self.nome = nome
@@ -17,9 +17,9 @@ class Aluno(Base):
         self.curso = curso
 
     def __repr__(self):
-        return f"Produto (id={self.id}, nome={self.nome}, idade={self.idade}, curso={self.curso})"
+        return f"Aluno (id={self.id}, nome={self.nome}, idade={self.idade}, curso={self.curso})"
 
-engine = create_engine("sqlite:///escola.db", echo=True)
+engine = create_engine("sqlite:///escola.db", echo=False)
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
@@ -33,14 +33,21 @@ aluno5 = Aluno("Raul", 17, "Dev. Sistemas")
 aluno6 = Aluno("Guilherme", 16, "Dev. Sistemas")
 aluno7 = Aluno("Sérgio", 45, "Culinária")
 aluno8 = Aluno("João", 27, "Boy band")
-session.add(aluno1)
-session.add(aluno2)
-session.add(aluno3)
-session.add(aluno4)
-session.add(aluno5)
-session.add(aluno6)
-session.add(aluno7)
-session.add(aluno8)
-session.commit()
+# session.add(aluno1)
+# session.add(aluno2)
+# session.add(aluno3)
+# session.add(aluno4)
+# session.add(aluno5)
+# session.add(aluno6)
+# session.add(aluno7)
+# session.add(aluno8)
+# session.commit()
 
-alunos = session.query(Aluno).all()
+# alunos = session.query(Aluno).all()
+# for i in alunos:
+#     print(f"Nome: {i.nome} | Idade: {i.idade} | Curso: {i.curso}")
+
+aluno_id = session.query(Aluno).filter(Aluno.id == 1).first()
+print(aluno_id)
+
+atualizar_aluno = session.query(Aluno).filter_by(id=1).first()
